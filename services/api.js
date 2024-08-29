9909b22a4bc65b3cd469073208a99edfcbda685d
carlosvale03 <dovalecarlos03@gmail.com>
2024-08-29 18:21:39 -0300

    adição da pagina de post pelo id

--- services/api.js
+++ services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+// password: WlAL0pimZjMNTBPr
+
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://cfqoskxskqkplirgioqd.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmcW9za3hza3FrcGxpcmdpb3FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ5NjE5MjMsImV4cCI6MjA0MDUzNzkyM30.iN0kjosUpV2EBK9SDRJ9ac90LwtmBI_TGmqmiOL_Po4",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmcW9za3hza3FrcGxpcmdpb3FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ5NjE5MjMsImV4cCI6MjA0MDUzNzkyM30.iN0kjosUpV2EBK9SDRJ9ac90LwtmBI_TGmqmiOL_Po4"
     }
 })
--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -1,7 +1,7 @@
 import { api } from '../services/api'
 
 export const getPosts = async () => {
-    const {data} = await api.get('/posts'); 
+    const {data} = await api.get('/posts');
 
     if(data){
         return data;
@@ -11,9 +11,24 @@ export const getPosts = async () => {
 }
 
 export const getPostBySlug = async (id) => {
-
+  try{
     //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
+    const { data, error } = await api.get(`/posts?id=eq.${id}`);
 
-    return {}
+    // Verificando se houve um erro na requisição
+    if (error) {
+      console.error('Erro ao buscar o post:', error);
+      return null;
+    }
+
+    // Retornando o primeiro resultado, caso o post seja encontrado
+    if (data && data.length > 0) {
+      return data[0]; // Retorna o post específico
+    } else {
+      return null; // Retorna null se não houver post com o ID fornecido
+    }
+  } catch (error) {
+    console.error('Erro na requisição à API:', error);
+    return null;
+  }
 }
